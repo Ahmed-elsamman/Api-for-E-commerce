48e8b50edf27f362c17cf477e2d6f5fd69b62c8d
Your Name <samman66512@gmail.com>
2025-08-02 17:58:11 +0300

    Refactor code structure for improved readability and maintainability

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,22 @@
-export default () => ({});
+// export default () => ({documentation: {
+//   enabled: true,
+// },});
+
+// path: config/plugins.js
+module.exports = ({ env }) => ({
+  'documentation': {
+    enabled: true,
+    config: {
+      // يمكنك تخصيصه حسب الحاجة
+      info: {
+        version: '1.0.0',
+        title: 'E-commerce API',
+        description: 'API Documentation using Swagger',
+      },
+      contact: {
+        name: 'Ahmed ElSamman',
+        email: 'your-email@example.com',
+      },
+    },
+  },
+});
